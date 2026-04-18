a386cf38be8731d07c1785e4478a9849be76517e
mastergodlygamerz <kamaljeet.amanat.singh@gmail.com>
2026-04-18 14:16:52 +0530

    Add class and board selection features, update UI with classes 7-12

---
+++ server.js
@@ -0,0 +1,28 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const server = http.createServer((req, res) => {
+  let filePath = req.url === '/' ? '/index.html' : req.url;
+  filePath = path.join(__dirname, filePath);
+  
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      res.writeHead(404);
+      res.end('File not found');
+      return;
+    }
+    
+    const ext = path.extname(filePath);
+    let contentType = 'text/html';
+    if (ext === '.js') contentType = 'text/javascript';
+    if (ext === '.css') contentType = 'text/css';
+    
+    res.writeHead(200, { 'Content-Type': contentType });
+    res.end(data);
+  });
+});
+
+server.listen(8000, () => {
+  console.log('Server running at http://localhost:8000/');
+});
